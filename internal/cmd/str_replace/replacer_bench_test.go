305ff6f920e96aa65c8dc1d5c3de314edac071fc
Adam P. Regasz-Rethy <adam.regaszrethy@shopify.com>
2025-06-20 21:17:16 -0400

    Add comprehensive benchmarks for view and str_replace commands

---
+++ internal/cmd/str_replace/replacer_bench_test.go
@@ -0,0 +1,160 @@
+package str_replace
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func BenchmarkReplacer_StrReplace(b *testing.B) {
+	tmpDir := b.TempDir()
+	
+	// Create test files of different sizes
+	sizes := []struct {
+		name string
+		size int
+	}{
+		{"small", 100},
+		{"medium", 10000},
+		{"large", 100000},
+	}
+	
+	for _, size := range sizes {
+		b.Run(size.name, func(b *testing.B) {
+			// Create file with specified number of lines containing target string
+			line := "this is a test line with hello in it\n"
+			content := strings.Repeat(line, size.size)
+			
+			r := &Replacer{}
+			
+			for i := 0; i < b.N; i++ {
+				b.StopTimer()
+				testFile := filepath.Join(tmpDir, "bench_"+size.name+"_"+string(rune(i))+".txt")
+				err := os.WriteFile(testFile, []byte(content), 0644)
+				if err != nil {
+					b.Fatal(err)
+				}
+				b.StartTimer()
+				
+				err = r.StrReplace(testFile, "hello", "hi")
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkReplacer_StrReplacePatterns(b *testing.B) {
+	tmpDir := b.TempDir()
+	
+	// Create a medium-sized file
+	line := "this is a test line with various patterns to replace\n"
+	content := strings.Repeat(line, 10000)
+	
+	patterns := []struct {
+		name   string
+		oldStr string
+		newStr string
+	}{
+		{"short_to_short", "is", "was"},
+		{"short_to_long", "a", "absolutely"},
+		{"long_to_short", "various patterns", "stuff"},
+		{"long_to_long", "test line with", "example string containing"},
+		{"no_match", "xyz", "abc"},
+	}
+	
+	for _, pattern := range patterns {
+		b.Run(pattern.name, func(b *testing.B) {
+			r := &Replacer{}
+			
+			for i := 0; i < b.N; i++ {
+				b.StopTimer()
+				testFile := filepath.Join(tmpDir, "bench_pattern_"+pattern.name+"_"+string(rune(i))+".txt")
+				err := os.WriteFile(testFile, []byte(content), 0644)
+				if err != nil {
+					b.Fatal(err)
+				}
+				b.StartTimer()
+				
+				err = r.StrReplace(testFile, pattern.oldStr, pattern.newStr)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkReplacer_StrReplaceFrequency(b *testing.B) {
+	tmpDir := b.TempDir()
+	
+	// Test different frequencies of target string
+	frequencies := []struct {
+		name       string
+		targetFreq int // every Nth word is the target
+	}{
+		{"rare", 1000},     // 1 in 1000 words
+		{"uncommon", 100},  // 1 in 100 words
+		{"common", 10},     // 1 in 10 words
+		{"frequent", 3},    // 1 in 3 words
+	}
+	
+	for _, freq := range frequencies {
+		b.Run(freq.name, func(b *testing.B) {
+			// Create content with target string at specified frequency
+			var contentBuilder strings.Builder
+			for i := 0; i < 10000; i++ {
+				if i%freq.targetFreq == 0 {
+					contentBuilder.WriteString("target ")
+				} else {
+					contentBuilder.WriteString("word ")
+				}
+			}
+			content := contentBuilder.String()
+			
+			r := &Replacer{}
+			
+			for i := 0; i < b.N; i++ {
+				b.StopTimer()
+				testFile := filepath.Join(tmpDir, "bench_freq_"+freq.name+"_"+string(rune(i))+".txt")
+				err := os.WriteFile(testFile, []byte(content), 0644)
+				if err != nil {
+					b.Fatal(err)
+				}
+				b.StartTimer()
+				
+				err = r.StrReplace(testFile, "target", "replacement")
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkStringOperations(b *testing.B) {
+	// Benchmark the core string operations used in str_replace
+	content := strings.Repeat("hello world hello universe hello galaxy\n", 10000)
+	
+	b.Run("strings_Count", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = strings.Count(content, "hello")
+		}
+	})
+	
+	b.Run("strings_ReplaceAll", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = strings.ReplaceAll(content, "hello", "hi")
+		}
+	})
+	
+	b.Run("combined_operations", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			modified := strings.ReplaceAll(content, "hello", "hi")
+			_ = strings.Count(content, "hello")
+			_ = modified
+		}
+	})
+}
---
+++ internal/cmd/view/viewer_bench_test.go
@@ -0,0 +1,146 @@
+package view
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func BenchmarkViewer_ViewFile(b *testing.B) {
+	tmpDir := b.TempDir()
+	
+	// Create test files of different sizes
+	sizes := []struct {
+		name string
+		size int
+	}{
+		{"small", 100},
+		{"medium", 10000},
+		{"large", 1000000},
+	}
+	
+	for _, size := range sizes {
+		b.Run(size.name, func(b *testing.B) {
+			// Create file with specified number of lines
+			content := strings.Repeat("this is a test line with some content\n", size.size)
+			testFile := filepath.Join(tmpDir, "bench_"+size.name+".txt")
+			err := os.WriteFile(testFile, []byte(content), 0644)
+			if err != nil {
+				b.Fatal(err)
+			}
+			
+			v := &Viewer{}
+			b.ResetTimer()
+			
+			for i := 0; i < b.N; i++ {
+				err := v.viewFile(testFile, "")
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkViewer_ViewFileWithRange(b *testing.B) {
+	tmpDir := b.TempDir()
+	
+	// Create large test file
+	content := strings.Repeat("this is a test line with some content\n", 100000)
+	testFile := filepath.Join(tmpDir, "bench_range.txt")
+	err := os.WriteFile(testFile, []byte(content), 0644)
+	if err != nil {
+		b.Fatal(err)
+	}
+	
+	ranges := []struct {
+		name  string
+		range_ string
+	}{
+		{"first_100", "1,100"},
+		{"middle_100", "50000,50100"},
+		{"last_100", "99900,100000"},
+		{"to_end", "99900,-1"},
+	}
+	
+	for _, r := range ranges {
+		b.Run(r.name, func(b *testing.B) {
+			v := &Viewer{}
+			b.ResetTimer()
+			
+			for i := 0; i < b.N; i++ {
+				err := v.viewFile(testFile, r.range_)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkViewer_ViewDir(b *testing.B) {
+	tmpDir := b.TempDir()
+	
+	// Create directories with different numbers of files
+	sizes := []struct {
+		name  string
+		files int
+	}{
+		{"small", 10},
+		{"medium", 100},
+		{"large", 1000},
+	}
+	
+	for _, size := range sizes {
+		b.Run(size.name, func(b *testing.B) {
+			testDir := filepath.Join(tmpDir, "bench_"+size.name)
+			err := os.Mkdir(testDir, 0755)
+			if err != nil {
+				b.Fatal(err)
+			}
+			
+			// Create files
+			for i := 0; i < size.files; i++ {
+				fileName := filepath.Join(testDir, "file"+string(rune(i))+".txt")
+				err := os.WriteFile(fileName, []byte("content"), 0644)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+			
+			v := &Viewer{}
+			b.ResetTimer()
+			
+			for i := 0; i < b.N; i++ {
+				err := v.viewDir(testDir)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkViewer_ParseRange(b *testing.B) {
+	v := &Viewer{}
+	
+	ranges := []string{
+		"",
+		"1,10",
+		"1000,2000",
+		"50,-1",
+		" 100 , 200 ",
+	}
+	
+	for _, r := range ranges {
+		b.Run("range_"+r, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_, _, err := v.parseRange(r)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
